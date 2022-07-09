28aa8d469a6b71f16ae8828f9aece37f9a1f824f
koreicnurs <koreicnurs@gmail.com>
2022-07-09 12:23:22 +0600

    add get all quotes and render on main page

--- src/containers/Quotes/Quotes.js
+++ src/containers/Quotes/Quotes.js
@@ -1,20 +1,69 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from "../../components/NavBar/NavBar";
+import axios from "axios";
+import {NavLink} from "react-router-dom";
+import Spinner from "../../UI/Spinner/Spinner";
+import Button from "../../UI/Button/Button";
 
 const Quotes = () => {
     const [quotes, setQuotes] = useState(null);
-    
-    const addCoutes = () => {
-      
-    }
-    
-    return (
+    const [loading, setLoading] = useState(false);
 
-        <>
-            <NavBar/>
+    const getPosts = async () => {
+        setLoading(true);
+        try {
+            const response = await axios(`https://bn-task-63-default-rtdb.europe-west1.firebasedatabase.app/quotes.json`);
+            const quotesArray = [];
+            if (response.data) {
+                for (let key of Object.entries(response.data)) {
+                    quotesArray.push({
+                        id: key[0],
+                        category: key[1].quote.category,
+                        author: key[1].quote.author,
+                        quoteText: key[1].quote.quoteText
+                    });
+                }
+            }
+            setQuotes(quotesArray);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-        </>
-    );
+
+    useEffect(() => {
+        getPosts().catch();
+    }, []);
+
+    return loading ? (<Spinner/>)
+        :
+        quotes && (
+            <>
+                <NavBar/>
+
+                <div className='posts'>
+                    {quotes.map(q => {
+                        return (
+                            <div className="card" key={q.author}>
+                                <div className="card-body">
+                                    <p className="card-text">{q.quoteText}</p>
+                                    <h6 className='card-title'>- {q.author}</h6>
+                                    <Button
+                                        classType='warning'
+                                        type='button'
+                                        // clicked={}
+                                        name='Edit'
+                                    />
+                                    <NavLink className="btn btn-danger" to={`posts/${q.id}`}>X</NavLink>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </>
+        );
 };
 
 export default Quotes;
